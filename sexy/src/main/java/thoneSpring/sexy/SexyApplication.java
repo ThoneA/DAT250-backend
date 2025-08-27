37ac48e18e09d7a86fddf2ca1fc029324adb93af
ThoneA <thone@ThonePc.localdomain>
2025-08-27 13:58:58 +0200

    lagt inn gradle

---
+++ sexy/src/main/java/thoneSpring/sexy/SexyApplication.java
@@ -0,0 +1,13 @@
+package thoneSpring.sexy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SexyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SexyApplication.class, args);
+	}
+
+}
---
+++ sexy/src/test/java/thoneSpring/sexy/SexyApplicationTests.java
@@ -0,0 +1,13 @@
+package thoneSpring.sexy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SexyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
